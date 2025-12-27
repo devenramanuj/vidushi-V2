c052b29ea1c8f1dca6dac3aa98748702faeaa295
devenramanuj <43030670+devenramanuj@users.noreply.github.com>
2025-12-27 21:41:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('vidushi-store').then((cache) => cache.addAll([
+      '/',
+      'index.html',
+      'manifest.json',
+      'silent.mp4',
+      'talking.mp4',
+      'smiling.mp4',
+      'thinking.mp4',
+      'angry.mp4'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
 
